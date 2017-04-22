79aad229e93b3e6ab387c7dac19e8d0cab2f7e36
Josue <j.encinar.87@gmail.com>
2017-04-22 20:38:22 +0200

    postal

--- php/mail.php
+++ php/mail.php
@@ -6,7 +6,6 @@
     $message = $_POST['message']; 
     $para = $_POST['email'];
     $nPostal = $_POST['postcard'];
-    $enlace = "https://castillodehurdes.000webhostapp.com/images/postal".split(" ",$nPostal)[1].".jpg";
 
     $header = 'From: ' . $mail . "\r\n"; 
     $header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
@@ -15,12 +14,9 @@
 
     $mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n"; 
     $mensaje .= "El contenido es: ". $message . " \r\n";
-    $mensaje .= "La postal que te envía es: <img src='$enlace'/>";
-    mensaje . = "Si no ve bien la postal pincha el siguiente <a href='$enlace'>enlace</a>"
     $mensaje .= "Enviado el " . date('d/m/Y', time()); 
 
     $asunto = $nombre . ' sends you a postcard'; 
-    echo "Postcard ". ;
 
     if (mail($para, $asunto, $mensaje,$headers,"-f".$mail)) {
         echo ("The email was send.");
@@ -30,4 +26,5 @@
 
     echo ("<a href='javascript:history.back(1)'>Back to postcard</a>");
 
-?> 
+   
+?>
