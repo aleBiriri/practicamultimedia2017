43c96bd81b96e59d4ef8bc66ab2b515f2d3fc2f6
aleBiriri <adovalepadron@gmail.com>
2017-04-24 23:14:12 +0200

    Versión final, salvo errores que se vayan encontrando y que haya que corregir

--- js/activities.js
+++ js/activities.js
@@ -4,10 +4,10 @@
 			for(var i=0; i<activities.length; i++){
 				console.log(activities[i]);
 				document.getElementById(activities[i]).style.display = "none";
-				document.getElementById("_"+activities[i]).style.color = "#89be5c";
+				document.getElementById("_"+activities[i]).style.color = "#3c7c06";
 			}
 			document.getElementById(id).style.display = "block";
-			document.getElementById("_"+id).style.color = "#497b25";
+			document.getElementById("_"+id).style.color = "#275704";
 		}
 
 		window.onload = loadActivity("activity0");
